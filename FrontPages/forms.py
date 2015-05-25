7040e5f2b2060338cbf1112a65fb0231374b8139
Schachte <rschacht@asu.edu>
2015-05-25 16:42:49 -0700

    Updated template, login WORKING

---
+++ FrontPages/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django import forms
+
+from crispy_forms.bootstrap import AppendedText, PrependedText
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, ButtonHolder, Submit
+from crispy_forms.layout import Field
+from crispy_forms.bootstrap import AppendedText, PrependedText
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, ButtonHolder, Submit
+from crispy_forms.layout import Field
+from django.contrib.auth.models import User
+
--- FrontPages/views.py
+++ FrontPages/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render, render_to_response, redirect, get_object_or_404, Http404
+from django.contrib.auth import authenticate, login
 from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 def indextest(request):
 	context = RequestContext(request)
-	return render(request, 'home.html', {})
+	loggedin = False
+	# If the request is a HTTP POST, try to pull out the relevant information.
+	if request.method == 'POST':
+
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = authenticate(username=username, password=password)
+		if user:
+			
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect('/')
+			else:
+				return HttpResponseRedirect('/error')
+		else:
+			loggedin = True
+			return render(request,
+			'home.html',
+			{'loggedin': loggedin} )
+	return render_to_response('home.html', {}, context)
+
+
